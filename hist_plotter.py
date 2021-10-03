379e14be138f0ce27efb3f77315ca41536f6d299
ImageProjection <artemulyanov2000@gmail.com>
2021-10-03 11:42:34 +0300

    Add not exacly working gauss ift (ift_v2.py)

--- hist_plotter.py
+++ hist_plotter.py
@@ -1,6 +1,9 @@
-import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.core.function_base import linspace
+from scipy.fftpack import fft,fftshift,fftfreq
+from scipy.fftpack.basic import ifft
+from scipy.stats import norm
 
 fig=plt.figure()
 ax1=fig.add_subplot(2,1,1)
@@ -14,16 +17,22 @@ for line in f:
     x_data.append(tmp_list[0])
     y_data.append(tmp_list[1])
 
+
+#for testing
+y_data=[]
+for i in range(NFFT):
+    y_data.append(norm.pdf((i-NFFT/2)/NFFT*10))
 ax1.set_ylabel("|P(x)|^2")
 ax1.set_xlabel("coordinate x")
 ax1.plot(x_data,y_data)
 ax1.grid(color = 'black', linestyle = '--', linewidth = 0.5)
 
+
 ax2=fig.add_subplot(2,1,2)
 sample_spacing=x_data[1]-x_data[0]
-ft_ydata=
-ft_xdata=
-ax2.plot()
+ift_ydata=ifft((y_data))
+ift_xdata=np.linspace(-1,1,NFFT,endpoint=False)
+ax2.plot(ift_xdata,ift_ydata)
 ax2.grid(color = 'green', linestyle = '--', linewidth = 0.5)
 plt.locator_params(nbins=40)
 plt.show()
--- ift_v1.py
+++ ift_v1.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.core.function_base import linspace # library for plotting
+from numpy.core.function_base import linspace
 from scipy.fftpack import fft,fftshift,fftfreq
 from scipy.fftpack.basic import ifft
+from scipy.stats import norm
 
 
 
-fft
 
 f = 5 #frequency = 10 Hz
 overSampRate = 30 #oversammpling rate
@@ -16,7 +16,8 @@ fs = f*overSampRate #sampling frequency
 NFFT=1024 #NFFT-point DFT      
 
 fVals=np.zeros(NFFT)
-fVals[511]=1
+for i in range(NFFT):
+    fVals[i]=norm.pdf((i-NFFT/2)/NFFT*10)
 #t=np.linspace(0,NFFT*1/fs,NFFT,endpoint=False)
 t=np.linspace(0,1,NFFT,endpoint=False)
 
