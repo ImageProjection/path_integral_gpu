f2a13daee17605ee5a16c3942653d43738f24d58
ImageProjection <artemulyanov2000@gmail.com>
2021-10-03 14:24:18 +0300

    Add more working ift examples and working hist plotter

--- hist_plotter.py
+++ hist_plotter.py
@@ -12,16 +12,18 @@ x_data=[]
 y_data=[]
 NFFT=0
 for line in f:
-    NFFT+=1#number of points for FFT to process
+    NFFT+=1#number of points for IFFT to process
     tmp_list=list(map(float,line.split(",")))
     x_data.append(tmp_list[0])
     y_data.append(tmp_list[1])
 
 
 #for testing
-y_data=[]
-for i in range(NFFT):
-    y_data.append(norm.pdf((i-NFFT/2)/NFFT*10))
+'''it works for this gauss
+y_data=np.zeros(NFFT)
+for i in range(250):
+    y_data[i]=norm.pdf((i-0)/20)
+'''
 ax1.set_ylabel("|P(x)|^2")
 ax1.set_xlabel("coordinate x")
 ax1.plot(x_data,y_data)
@@ -29,10 +31,9 @@ ax1.grid(color = 'black', linestyle = '--', linewidth = 0.5)
 
 
 ax2=fig.add_subplot(2,1,2)
-sample_spacing=x_data[1]-x_data[0]
-ift_ydata=ifft((y_data))
+ift_ydata=ifft(y_data)
 ift_xdata=np.linspace(-1,1,NFFT,endpoint=False)
-ax2.plot(ift_xdata,ift_ydata)
+ax2.plot(ift_xdata[400:600],fftshift(ift_ydata)[400:600])
 ax2.grid(color = 'green', linestyle = '--', linewidth = 0.5)
 plt.locator_params(nbins=40)
 plt.show()
---
+++ ift_v3gauss.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.function_base import linspace
+from scipy.fftpack import fft,fftshift,fftfreq
+from scipy.fftpack.basic import ifft
+from scipy.stats import norm
+
+
+
+
+
+NFFT=1024 #NFFT-point DFT      
+fVals_half=np.zeros(512)#amplitudes for each frequency
+#fVals=np.zeros(NFFT)
+for i in range(250):
+    fVals_half[i]=norm.pdf((i-0)/10)
+fVals=np.concatenate((fVals_half,np.zeros(512)))
+#fVals=np.concatenate((fVals_half,np.flip(fVals_half)))
+
+#t=np.linspace(0,NFFT*1/fs,NFFT,endpoint=False)
+t=np.linspace(0,1,NFFT,endpoint=False)
+
+x=ifft(fVals,NFFT)
+
+fig=plt.figure()
+ax1=fig.add_subplot(311)
+ax2=fig.add_subplot(312)
+ax3=fig.add_subplot(313)
+
+ax1.plot(fVals)
+ax1.set_title('spectrum without fftshift')
+
+ax2.plot(fftshift(fVals))
+ax2.set_title('spectrum with fftshift')
+
+ax3.plot(t,fftshift(x))
+ax3.set_xlabel('Time (s)')
+ax3.set_ylabel('signal value')
+plt.show()
