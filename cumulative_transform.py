296d1d48f067ea8ba80b4dd63020906f244b36b1
ImageProjection <artemulyanov2000@gmail.com>
2022-03-27 14:49:24 +0300

    Add cumulative transform file

---
+++ cumulative_transform.py
@@ -0,0 +1,4 @@
+'''
+input: p-trajectories file
+output: x trajectories file of same format
+'''
--- p_x_plot_densities.py
+++ p_x_plot_densities.py
@@ -1,5 +1,5 @@
 '''
-input: p-trajectories file
+input: p and x trajectories files and gen_params_file
 output: -p trajectories density plot
         -x trajectories density plot (evaluated here using cumulative transform)
 '''
