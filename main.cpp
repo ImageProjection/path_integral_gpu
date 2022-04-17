503c18fcd59919d5de66722ac2b82b011c59029b
ImageProjection <artemulyanov2000@gmail.com>
2022-04-17 19:52:17 +0300

    Found sort of optimal evolution speed

--- main.cpp
+++ main.cpp
@@ -331,7 +331,7 @@ int main(int argc, char *argv[])
 	//termo parameters
 	const int N_waiting_trajectories=85; //number of Metropolis steps to termolise the system
 	const int N_sample_trajectories=320;//this many traj-s are used to build histogram
-	const int N_steps_per_traj=600;//this many metropolis propositions are made for each of this traj-s
+	const int N_steps_per_traj=300;//this many metropolis propositions are made for each of this traj-s
 	double beta=3;//atof(argv[1]);
 	//int n_periods=atoi(argv[2]); its for testing p_b
 	double a=0.015;//0.035*2;
