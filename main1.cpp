f1f9bb6bd420b427a587c95d51514850031fd3ab
ImageProjection <artemulyanov2000@gmail.com>
2022-04-22 21:05:39 +0300

    Another commit cause i want

--- main1.cpp
+++ main1.cpp
@@ -20,9 +20,9 @@ const double v_fermi=1;
 const double a=0.01;
 const double dtau=0.02;//epsilon for md
 const double beta= a*N;
-const int N_waiting_trajectories=1;//100
-const int N_sample_trajectories=4;//40000 for 15 min
-const int N_steps_per_traj=2;//200
+const int N_waiting_trajectories=100;//100
+const int N_sample_trajectories=40000;//40000 for 15 min
+const int N_steps_per_traj=200;//200
 
 const int T_md=20;
 
